359d58bce606c65cd810615ac1a6f25dd914874b
Vikash Kumar <vkd3mynds@gmail.com>
2024-07-29 10:58:47 +0530

    1st push

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+const App = ()=>{
+    return(
+        <>
+        <h1 className="text-blue-700 text-5xl">WEBPACK FILES</h1>
+        </>
+    )
+}
+
+export default App;
